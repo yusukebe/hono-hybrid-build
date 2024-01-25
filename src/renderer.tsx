28a4eed84666c93bc2d716a9d5c481a12f5066a0
Yusuke Wada <yusuke@kamawada.com>
2024-01-25 23:09:06 +0900

    initial commit

---
+++ src/renderer.tsx
@@ -0,0 +1,25 @@
+import 'hono'
+import { jsxRenderer } from 'hono/jsx-renderer'
+
+declare module 'hono' {
+  interface ContextRenderer {
+    (content: string | Promise<string>, props?: { title?: string }): Response
+  }
+}
+
+export const renderer = jsxRenderer(
+  ({ children, title }) => {
+    return (
+      <html>
+        <head>
+          <link href="/static/style.css" rel="stylesheet" />
+          <title>{title}</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    )
+  },
+  {
+    docType: true
+  }
+)
